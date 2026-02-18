29c0b5d68ead51312f7f39dc37b11d29320160f2
Ralph Boter <ralph.boter99@gmail.com>
2026-02-18 15:55:54 +0800

    Update server.go

--- server.go
+++ server.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+type userInfo struct {
+	userID   string `json:"id"`
+	username string `json:"username"`
+	userAge  string `json:"age"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+var users []userInfo
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var newUser userInfo
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+	users = append(users, newUser)
+	json.NewEncoder(w).Encode(newUser)
 }
